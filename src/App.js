b55e005f1788a9c9a57a23e77d01ce3f8403b4ad
Asad Yasin <51986208+asadyasin@users.noreply.github.com>
2022-10-15 23:27:37 -0700

    change name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+const App = () => {
+  const name = "Asad Yasin";
+  const isNameShowing = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, {isNameShowing ? name : "Phudo"}</h1>
+      {isNameShowing ? (<>
+            Hello
+          <h1>Khallo</h1>
+
+        </>):(
+          <>
+          
+          <h1>Khallo</h1>
+          <h1>Khallo</h1>
+          </>
+        )
+        }
     </div>
   );
 }
